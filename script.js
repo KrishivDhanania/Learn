82ae14e3fea836edb269fb6b2359721a76255ed9
Krishiv Dhanania <dhananiakrishiv@gmail.com>
2023-03-05 19:16:47 +0530

    <script> var typed = new Typed('.autotype', { strings: [ "Faltu" , "HTML" , "CSS" , "JavaScript" , "Java" , "Python" , "Ruby" ,"Angular" , "And More "  ], typeSpeed: 30 , backSpeed: 30 , loop: true });
    </script>

---
+++ script.js
@@ -0,0 +1,10 @@
+<script>
+var typed = new Typed('.autotype', {
+strings: [ "Faltu" , "HTML" , "CSS" , "JavaScript" , "Java" , "Python" , "Ruby" ,"Angular" , "And More "  ],
+typeSpeed: 30 ,
+backSpeed: 30 ,
+loop: true
+});
+
+   
+</script>
