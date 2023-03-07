b835076849b26162f3c399f68b72ef771c2ffdb9
Krishiv Dhanania <dhananiakrishiv@gmail.com>
2023-03-07 19:35:50 +0530

    h1 {
     font-size: 30px;
    
    }
    button{
        font-size: 25px;
         border: 5px red solid;
         background-color: black;
         color: antiquewhite;
         transition-property: background-color ;
         transition-duration: 1s;
         transition-timing-function: ease-in-out ;
    }
    button:hover{
        background-color: yellow;
        color: black;
    }
    img{
        width: 100px;
        height: 100px;
    }
    
    nav{
        display: inline-block;
        background-color: red;
    
    }
    *{
        background-color: rgb(100, 100, 231);
        color: antiquewhite;
    }
    hr{
        height: 10px; background-color: black;
    }

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
 
-var typed = new Typed('.autotype', {
-strings: [ "Faltu" , "HTML" , "CSS" , "JavaScript" , "Java" , "Python" , "Ruby" ,"Angular" , "And More "  ] ,
-typeSpeed: 30 ,
-backSpeed: 30 ,
-loop: true ,
-})
